dda664803551f78c2dfa5231aecf8fee3e742207
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-10 12:29:27 +0200

    exercises FBV - update

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0l2xd=@)&&2tloj$0xs9g04sy9t2yr@nfnjtpp=m7_!cu2b^7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
