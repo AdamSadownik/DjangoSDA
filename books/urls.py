51f1328457358d24c17b4800002718f80ea6d287
Uzytkownik <uzytkownik@kursGIT.pl>
2022-04-24 12:08:04 +0200

    BooksListView class added + pagination

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from books.views import get_uuids_a, get_uuids_b, get_fun1, get_fun2, get_argument_from_path, \
     get_arguents_from_query, check_http_query_type, get_headers, raise_error_for_fun, AuthorListBaseView, \
-    CategoryListTemplateView
+    CategoryListTemplateView, BooksListView
 
 urlpatterns = [
     path('uuids-a', get_uuids_a),
@@ -16,4 +16,5 @@ urlpatterns = [
     path('raise-error', raise_error_for_fun, name='raise-error'),
     path('author_list', AuthorListBaseView.as_view(), name='author_list'),
     path('category_list', CategoryListTemplateView.as_view(), name='category_list'),
+    path('books_list', BooksListView.as_view(), name='books_list'),
 ]
