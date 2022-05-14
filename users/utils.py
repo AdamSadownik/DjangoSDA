6783dd8e3aebdc408312e75a63ce8ed586142b08
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-14 13:58:31 +0200

    verification_password_without_salt function was added

--- users/utils.py
+++ users/utils.py
@@ -1,5 +1,9 @@
 import hashlib
 
 def hash_password_without_salt(password: str) -> str:
-    hashPass = hashlib.sha256(password.encode("ascii")).hexdigest()
-    return hashPass
+    hash_pass = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return hash_pass
+
+def verification_password_without_salt(password: str, hash_pass: str) -> bool:
+    input_pass = hashlib.sha256(password.encode("ascii")).hexdigest()
+    return input_pass == hash_pass
