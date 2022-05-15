797de507f746ee93decd41c347badaf83b2ad1fd
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-15 11:13:43 +0200

    Login and logout functions added

--- books/views.py
+++ books/views.py
@@ -1,10 +1,10 @@
 from uuid import uuid4
 
-from django.core.exceptions import BadRequest
+from django.core.exceptions import BadRequest, PermissionDenied
 from django.core.handlers.wsgi import WSGIRequest
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView, ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
@@ -95,8 +95,16 @@ class BookDeleteView(DeleteView):
         return get_object_or_404(Book, id=self.kwargs.get("pk"))
 
 def get_hello(request: WSGIRequest) -> HttpResponse:
-    hello = "Hello world"
-    return render(request, template_name='hello_world.html', context={"hello_var": hello})
+    user: User = request.user # type: ignore
+    # password = None if user.is_anonymous else user.password
+    # email = None if user.is_anonymous else user.email
+    # date_joined = None if user.is_anonymous else user.date_joined
+    if not user.is_authenticated:
+        # raise PermissionDenied(
+        return HttpResponseRedirect(reverse('login'))
+    is_auth: bool = user.is_authenticated
+    hello = f"Hello {user.username}. Your password is: {user.password}, e-mail address: {user.email}, date joined: {user.date_joined}"
+    return render(request, template_name='hello_world.html', context={"hello_var": hello, "is_authenticated": is_auth})
 
 
 def get_uuids_a(request: WSGIRequest) -> HttpResponse:
--- users/utils.py
+++ users/utils.py
@@ -1,4 +1,6 @@
 import hashlib
+import os
+
 
 def hash_password_without_salt(password: str) -> str:
     hash_pass = hashlib.sha256(password.encode("ascii")).hexdigest()
@@ -7,3 +9,28 @@ def hash_password_without_salt(password: str) -> str:
 def verification_password_without_salt(password: str, hash_pass: str) -> bool:
     input_pass = hashlib.sha256(password.encode("ascii")).hexdigest()
     return input_pass == hash_pass
+
+def hash_password_with_salt(password: str) -> str:
+    # creating salt
+    random_size = 30
+    salt = os.urandom(random_size)
+    hashed_salt = hashlib.sha256(salt).hexdigest()
+
+    # hashing_password
+    new_password = password + hashed_salt
+    hashed_password = hashlib.sha256(new_password.encode('ascii')).hexdigest()
+
+    combined_salt_and_password = ''.join([hashed_password, hashed_salt])
+
+    return combined_salt_and_password
+
+def verify_password_with_salt(password: str, hashed_password: str) -> bool:
+    hashed_salt = hashed_password[-64:]
+    new_password = password + hashed_salt
+    password_to_check = hashlib.sha256(new_password.encode('ascii')).hexdigest()
+
+    combined_salt_and_password = ''.join([password_to_check, hashed_salt])
+
+    return combined_salt_and_password == hashed_password
+
+'2d6f7756a01805472ff1e206f32ce4d317aa86e7b08c3573a862cc45954ac916df23e2c4b31f185e97b2390a4eceb0b4d309ceca570c6953381e0b11697d2e17'
