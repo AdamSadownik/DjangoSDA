700c117f3cab97565dcb01dadb779ab09e46418a
Uzytkownik <uzytkownik@kursGIT.pl>
2022-05-15 11:42:41 +0200

    Login' and 'Sign up' buttons were added

--- books/views.py
+++ books/views.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 
+from django.contrib.auth.decorators import login_required
 from django.core.exceptions import BadRequest, PermissionDenied
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
@@ -94,14 +95,15 @@ class BookDeleteView(DeleteView):
     def get_object(self, **kwargs):
         return get_object_or_404(Book, id=self.kwargs.get("pk"))
 
+@login_required
 def get_hello(request: WSGIRequest) -> HttpResponse:
     user: User = request.user # type: ignore
     # password = None if user.is_anonymous else user.password
     # email = None if user.is_anonymous else user.email
     # date_joined = None if user.is_anonymous else user.date_joined
-    if not user.is_authenticated:
-        # raise PermissionDenied(
-        return HttpResponseRedirect(reverse('login'))
+    # if not user.is_authenticated:
+    #     # raise PermissionDenied(
+    #     return HttpResponseRedirect(reverse('login'))
     is_auth: bool = user.is_authenticated
     hello = f"Hello {user.username}. Your password is: {user.password}, e-mail address: {user.email}, date joined: {user.date_joined}"
     return render(request, template_name='hello_world.html', context={"hello_var": hello, "is_authenticated": is_auth})
